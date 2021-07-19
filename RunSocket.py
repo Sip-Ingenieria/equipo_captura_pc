1820f5867bb5e2b02807cb52f7842ba52d5e0624
Jhon Guerrero <jhon.guerrero@sipingenieria.com>
2021-07-19 19:13:58 +0000

    'mejoras_en_sistema'

--- RunSocket.py
+++ RunSocket.py
@@ -7,20 +7,24 @@ import log
 def consultar():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = s.connect_ex((configuracion.DIRECCION_IP_BLOQUEO, configuracion.PUERTO_SOCKET_BLOQUEO))
+    log.logging.error('el codigo que retorna la conexion es: %s' % result)
+    print('el codigo que retorna la conexion es: %s' % result)
     if result == 0:
-       return True
+        return True
+    elif result == 10061:
+        return False
     else:
-       return False
+        return True
 
 
 def abrir_puerto():
-
     while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.bind((configuracion.DIRECCION_IP_BLOQUEO, configuracion.PUERTO_SOCKET_BLOQUEO))
             s.listen(1)
             sc, addr = s.accept()
+            s.close()
         except:
             log.logging.error("timeout")
             pass
