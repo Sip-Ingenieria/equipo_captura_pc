a68acd916fbe383681e34542716e59274f087e90
Jhon Guerrero <jhon.guerrero@sipingenieria.com>
2022-02-23 21:59:32 +0000

    cambio etiqueta

--- modulos/impresora/ImpresoraZebra.py
+++ modulos/impresora/ImpresoraZebra.py
@@ -21,33 +21,56 @@ class ImpresoraZebra:
             "^FD %s^FS" % codigo
 
         else:
-            codigo_barras = "^BQN, 2, 5" \
-            "^FDHM,A%s^FS" % codigo
+            codigo_barras = "^BQN, 2, 2" \
+                            "^FDHM,A ETIQ: {n_etiqueta} " \
+                            "REF: {referencia} " \
+                            "LARG: {largo} " \
+                            "ANCH: {ancho} " \
+                            "PESO: {peso} " \
+                            "CANT: {cantidad} " \
+                            "PED: {op} " \
+                            "^FS".format(referencia=testo, largo=largo, ancho=ancho,
+                                         peso=peso, cantidad=cantidad, op=descripcion, n_etiqueta=n_etiqueta)
 
         message = "^XA" \
-                  "^FO5, 20^ADN, 36, 20^FD %s ^FS" \
-                  "^FO500, 20^ADN 26, 11^FD # ETIQUETA ^FS" \
-                  "^FO500, 40^ADN, 28, 15^FD %s ^FS" \
-                  "^FO30, 90^ADN 26, 11^FD CODIGO: ^FS" \
-                  "^FO120, 90^ADN, 26, 11^FD %s ^FS" \
-                  "^FO30, 130^ADN, 26, 11^FD %s ^FS" \
-                  "^FO30, 170^ADN, 26,11^FD PED: ^FS" \
-                  "^FO120, 170^ADN, 26, 11^FD %s ^FS" \
-                  "^FO300, 170^ADN, 26, 11^FD OT: ^FS" \
-                  "^FO380, 170^ADN, 26, 11^FD %s ^FS" \
-                  "^FO30, 210^ADN, 26, 11^FD CANT: ^FS" \
-                  "^FO120, 210^ADN, 36, 20^FD %s ^FS" \
-                  "^FO300, 210^ADN, 26, 11^FD PESO: ^FS" \
-                  "^FO380, 210^ADN, 36, 20^FD %s %s ^FS" \
-                  "^FO30, 270^ADN. 26, 11^FD ANCHO: ^FS" \
-                  "^FO120, 270^ADN, 36, 20^FD %s ^FS" \
-                  "^FO30, 330^ADN. 26, 11^FD LARGO: ^FS" \
-                  "^FO120, 330^ADN, 36, 20^FD %s ^FS" \
-                  "^FO400, 270^ADN, 36, 20" \
-                  "%s"\
-                  "^XZ" % (nombre_empresa,
-                           n_etiqueta, testo, nombre_producto, descripcion,
-                           ot, cantidad, peso, unidades[0], ancho, largo, codigo_barras)
+                  "^LH 129,10" \
+                  "^FO0, 010^ADN, 26, 11^FD REF : ^FS" \
+                  "^FO65, 10 ^ADN, 26, 11^FD {referencia}^FS" \
+                  "^FO0, 40^ADN, 26, 11^FD LARG: ^FS" \
+                  "^FO65, 40^ADN, 26, 11^FD{largo}^FS" \
+                  "^FO0, 70^ADN, 26, 11^FD ANCH: ^FS" \
+                  "^FO65, 70^ADN, 26, 11^FD{ancho}^FS" \
+                  "^FO0, 100^ADN, 26, 11^FD PESO: ^FS" \
+                  "^FO65, 100^ADN, 26, 11^FD{peso}^FS" \
+                  "^FO0, 130^ADN, 26, 11^FD CANT: ^FS" \
+                  "^FO65, 130^ADN, 26, 11^FD{cantidad}^FS" \
+                  "^FO0, 160^ADN, 26, 11^FD PED : ^FS" \
+                  "^FO65, 160^ADN, 26, 11^FD {op}^FS" \
+                  "^FO50, 173^ADN, 36, 20" \
+                  "{codigo_qr_bar}" \
+                  "^LH 100,50" \
+                  "^FWB" \
+                  "^FO0, 010^AD, 26, 11^FD #ETIQUETA: {n_etiqueta}^FS" \
+                  "^FWR" \
+                  "^FO435, 0^AD, 26, 11^FD #ETIQUETA: {n_etiqueta}^FS" \
+                  "^LH 129,10" \
+                  "^FWN" \
+                  "^FO200, 010^ADN, 26, 11^FD REF : ^FS" \
+                  "^FO265, 10 ^ADN, 26, 11^FD {referencia}^FS" \
+                  "^FO200, 40^ADN, 26, 11^FD LARG: ^FS" \
+                  "^FO265, 40^ADN, 26, 11^FD{largo}^FS" \
+                  "^FO200, 70^ADN, 26, 11^FD ANCH: ^FS" \
+                  "^FO265, 70^ADN, 26, 11^FD{ancho}^FS" \
+                  "^FO200, 100^ADN, 26, 11^FD PESO: ^FS" \
+                  "^FO265, 100^ADN, 26, 11^FD{peso}^FS" \
+                  "^FO200, 130^ADN, 26, 11^FD CANT: ^FS" \
+                  "^FO265, 130^ADN, 26, 11^FD{cantidad}^FS" \
+                  "^FO200, 160^ADN, 26, 11^FD PED : ^FS" \
+                  "^FO265, 160^ADN, 26, 11^FD {op}^FS" \
+                  "^FO250, 173^ADN, 36, 20" \
+                  "{codigo_qr_bar}" \
+                  "^XZ".format(referencia=testo, largo=largo, ancho=ancho, peso=peso, cantidad=cantidad,
+                               op=descripcion,codigo_qr_bar=codigo_barras, n_etiqueta=n_etiqueta)
 
         message_byte = message.encode('utf-8')
 
@@ -66,13 +89,59 @@ class ImpresoraZebra:
         n_etiqueta = diccionario['n_etiqueta']
         codigo_barras = diccionario['codigo']
         nombre_producto = diccionario['n_producto']
-        unidades_ancho = 'm'
-        unidades_largo = 'm'
+        unidades_ancho = 'MTS'
+        unidades_largo = 'MTS'
+        unidades_cantidad = 'UND'
+
+        # fromatear valores
+        # valores del peso
+        peso_str = self.formato(peso, unidades, und_peso=True)
+        # valores metros
+        largo_str = self.formato(float(largo[:-2]), unidades_largo)
+        ancho_str = self.formato(float(ancho[:-2]), unidades_ancho)
+        # valores de cantidad
+        cantidad_str = self.formato(float(cantidad), unidades_cantidad)
 
         etiqueta = self.etiqueta(nombre_empresa, n_etiqueta, codigo, pedido, ot,
-                                 cantidad, peso, unidades, ancho, unidades_ancho, largo, unidades_largo,
-                                 codigo_barras,nombre_producto, codigo_b='QR')
+                                 cantidad_str, peso_str, unidades, ancho_str, unidades_ancho, largo_str, unidades_largo,
+                                 codigo_barras, nombre_producto, codigo_b='QR')
         return etiqueta
+
+    def formato(self, valor, unidades, und_peso=False):
+
+        # valores fijos
+        max_leng = 10
+        max_und_str = 3
+        espacio_vacio = ' '
+
+        # transformar valor en str con un espacio y 2,1,0 puntos flotantes
+        if (valor > 0) & (valor <= 99):
+            print(1)
+            n_strin = ' {:,.2f}'.format(valor)
+        elif (valor > 100) & (valor <= 999):
+            print(2)
+            n_strin = ' {:,.1f}'.format(valor)
+        else:
+            print(3)
+            n_strin = ' {:.0f}'.format(valor)
+
+        print (n_strin)
+
+        if und_peso is True:
+            if unidades.upper() == 'KILOS':
+                unidades = 'KG'
+            elif unidades.upper() == 'GRAMOS':
+                unidades = 'G'
+            else:
+                unidades = 'TON'
+
+        # mirar cuantos espacios agregar
+        multi = max_leng - len(n_strin) - max_und_str
+        # contruir valor con uniades
+        valor_frm = n_strin + (espacio_vacio * multi) + unidades
+        len(valor_frm)
+
+        return valor_frm
 
     def imprimir_ethernet(self, etiqueta):
         mysocket = self.mysocket
